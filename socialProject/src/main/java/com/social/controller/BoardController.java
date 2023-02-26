d4d2e9b05c69487b70cd0a0ec6f70f5e0e49a33e
이준성 <jun130506@gmail.com>
2023-02-26 15:55:44 +0900

    application 위치 변경

--- socialProject/src/main/java/com/social/controller/BoardController.java
+++ socialProject/src/main/java/com/social/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.social.controller;
 import com.social.dto.Board;
 import com.social.mapper.BoardMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+@SpringBootApplication
 @Controller
 public class BoardController {
 
